dd6106d292eaeb7c8c445611c62fb1e6fb5a95ed
Jason-West1013 <jasonwest1013@yahoo.com>
2019-08-02 23:20:57 -0400

    feat(Nav): create nav
    Create nav component and add service for mobile responsiveness.

---
+++ src/app/shared/responsive.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+// TODO: may want to add other screen sizes to test for....
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponsiveService {
+  private isMobile: boolean;
+
+  constructor() {
+    this.checkWidth();
+  }
+
+  getMobileStatus(): boolean {
+    return this.isMobile
+  }
+
+  checkWidth() {
+    const width = window.innerWidth;
+    if (width <= 768) {
+      this.isMobile = true
+    } else {
+      this.isMobile = false
+    }
+  }
+
+}
