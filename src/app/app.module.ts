ed99b0961f048ff63a9e76496e838f73e1b7ba19
Jason West <jasonwest1013@yahoo.com>
2019-08-05 22:44:14 -0400

    feat(Nav): add mobile navigation
    Add mobile navigation menu with animations and correct routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { RecipesModule } from './pages/recipes/recipes.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HomeModule,
     AboutModule,
     StandsModule,
--- src/app/shared/header/mobile-header/mobile-header.component.ts
+++ src/app/shared/header/mobile-header/mobile-header.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
-  styleUrls: ['./mobile-header.component.scss']
+  styleUrls: ['./mobile-header.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('350ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('350ms ease-in', style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class MobileHeaderComponent implements OnInit {
-  navMenuOpened: boolean = false;
+  navMenuOpened = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  openNavMenu() {
-    this.navMenuOpened = !this.navMenuOpened
+  toggleNavMenu() {
+    this.navMenuOpened = !this.navMenuOpened;
   }
 
 }
